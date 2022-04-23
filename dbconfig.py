9c92591d1daf3b87585360855dbba69049a1e468
Thepicolo <34195918+Jorgeablade@users.noreply.github.com>
2022-04-23 15:01:46 +0200

    Delete dbconfig.py

--- dbconfig.py
+++
@@ -1,6 +0,0 @@
-dbconfig = {
-            'Driver' : 'SQL Server Native Client 11.0',
-            'Server' : 'LAPTOP-M4KMV8R5\JORGESQL',
-            'Database' : 'CRUD',
-            'Trusted_Connection' : 'yes',
-}
